bb1d02c64a056fd6f9a11b3c9e9e13a98a595e0a
baarosh <norbert.lipinski96@gmail.com>
2020-11-22 16:11:37 +0100

    created obstacles

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ class Obstacle {
         this.visual.style.left = `${this.left}px`
 
         gameBoard.appendChild(this.visual)
+
+        this.active = true
     }
 }
 class Bird {
@@ -38,19 +40,22 @@ class Bird {
 const [screenWidth, screenHeight] = [1000, 650]
 const [birdWidth, birdHeight] = [50, 50]
 const [obstacleWidth, obstacleHeight] = [50, 200]
-const intervalSpeed = 100
+const intervalSpeed = 30
 const fallingSpeed = 5
-const jumpingSpeed = 20
-const movingSpeed = 10
+const jumpingSpeed = 10
+const movingSpeed = 7
 const jumpingHeight = 100
 const obstacles = []
+const creatingDistance = 800
 
 let fallingId
 let jumpingId
 let movingId
 
+let score = 0
 let canJump = false
 let isJumping = false
+let isCreating = false
 
 const falling = () => {
     fallingId = setInterval(() => {
@@ -68,11 +73,17 @@ const falling = () => {
 
 const jumping = () => {
     clearInterval(fallingId)
-    const currentHeight = bird.bottom + bird.height
+    const startingHeight = bird.bottom + bird.height
     jumpingId = setInterval(() => {
-        if (currentHeight + jumpingHeight > bird.bottom + bird.height) {
+    const currentHeight = bird.bottom + bird.height
+        if (startingHeight + jumpingHeight > currentHeight
+            && currentHeight <= screenHeight - jumpingSpeed) {
             bird.bottom += jumpingSpeed
             bird.visual.style.bottom = `${bird.bottom}px`
+            if(bird.bottom + bird.height >= screenHeight) {
+                clearInterval(jumpingId)
+                falling()
+            }
         }
         else {
             clearInterval(jumpingId)
@@ -87,11 +98,32 @@ const createAndMoveObstacles = () => {
 
     movingId = setInterval(() => {
         if(bird.bottom > 0) {
-            obstacles.forEach((obst) => {
+            obstacles.forEach((obst, index, obArray) => {
                 obst.left -= movingSpeed
                 obst.visual.style.left = `${obst.left}px`
 
+                if(
+                        bird.bottom <= obst.height
+                    &&  bird.left + bird.width >= obst.left
+                    &&  bird.left <= obst.left + obst.width
+                ) {
+                    clearInterval(movingId)
+                    clearInterval(fallingId)
+                    document.removeEventListener('keydown', listenerKeyDown)
+                    document.removeEventListener('keyup', listenerKeyUp)
+                }
 
+                if(index === obArray.length - 1
+                    &&  obst.left < creatingDistance + movingSpeed
+                    &&  obst.left > creatingDistance - movingSpeed) {
+                        const newObstacle = new Obstacle(obstacleWidth, obstacleHeight)
+                        obstacles.push(newObstacle)
+                }
+
+                if(obst.left + obst.width < bird.left && obst.active) {
+                    console.log(++score)
+                    obst.active = false
+                }
             })
         }
     },intervalSpeed)
